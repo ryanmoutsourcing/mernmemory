51bfdf1459581525d5de1cc5bc55138c72a6a39f
ryan.m.outsourcing <koreankid2@protonmail.com>
2022-06-29 04:06:10 -0300

    React and MongoDB works

--- server/index.js
+++ server/index.js
@@ -15,10 +15,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// Connect MongoDB
+// Connect MongoDB cool?
 
 const CONNECTION_URL = 'mongodb://localhost:27017/';
 const PORT = process.env.PORT || 5000;
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`) ))
