ce94202bc0fdfbb6e132b27f749dc6c2855c53be
ryan.m.outsourcing <koreankid2@protonmail.com>
2022-06-29 04:36:47 -0300

    postAPIWorks

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
-import { getPosts, createPost } from '../controllers/posts.js'
-
 const router = express.Router();
 
-router.get('/', getPosts);
-router.post('/', createPost);
+router.get('/', (req, res) => {
+	res.send('THIS WORKS!');
+});
 
 export default router;
